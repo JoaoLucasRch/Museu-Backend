b9e27600b7bc96e2afea8952e2fcc0632eb219f4
Gustavo Souza Carvalho <gusttavoo5080@gmail.com>
2025-12-12 22:44:37 -0300

    cadastro v1

---
+++ front-end/src/pages/Register/Register.tsx
@@ -0,0 +1,10 @@
+import AuthLayout from '../../components/authentication/AuthLayout';
+import RegisterForm from '../../components/authentication/RegisterForm';
+
+export default function Register() {
+  return (
+    <AuthLayout>
+      <RegisterForm />
+    </AuthLayout>
+  );
+}
